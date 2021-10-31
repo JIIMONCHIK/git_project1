d5bc8ee0d9088317d6b0ba9871fafc3591d783bf
JIIMONCHIK <artmvas38@Gmail.com>
2021-10-31 22:51:50 +0300

    Piano sound1

--- MIDI.py
+++ MIDI.py
@@ -5,6 +5,8 @@ import datetime
 
 from MidiKeyboardConnection import *
 from KeySoundConnection import *
+from ComputerKeyboardConnection import *
+from PyQt5.QtCore import Qt
 from PyQt5 import QtCore, QtMultimedia
 from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem
 from First import Ui_MainWindow
@@ -14,7 +16,7 @@ class Window(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.path = 'C:/Users/ASUS FX505DT/PycharmProjects/git_project1/Piano'
+        self.path = 'C:/Piano'
         self.format = '.m4a'
         self.pause = 0
         self.start_playing = False
@@ -23,6 +25,7 @@ class Window(QMainWindow, Ui_MainWindow):
         self.player = QtMultimedia.QMediaPlayer()
 
     def run(self):
+        print(self.sender())
         if self.start_playing:
             pause_time = datetime.datetime.now() - self.pause
             print('pause', pause_time.total_seconds())
@@ -37,6 +40,9 @@ class Window(QMainWindow, Ui_MainWindow):
         press_time = datetime.datetime.now() - self.sender().time
         print(press_time.total_seconds())
         self.player.stop()
+
+    def keyPressEvent(self, event):
+        keyboard_pressed(self, event)
 
 
 def except_hook(cls, exception, traceback):
