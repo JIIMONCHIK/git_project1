d3a62308e6374e8c602cc58dfbc6aa213f77e510
JIIMONCHIK <artmvas38@Gmail.com>
2021-11-03 12:15:59 +0300

    Record preview + sound choose bug fixed

--- ChooseSound.py
+++ ChooseSound.py
@@ -3,5 +3,7 @@ from PyQt5.QtWidgets import QFileDialog
 
 def choose_sound(self):
     self.fPath = QFileDialog.getOpenFileName(self, 'Выбрать файл', './')[0]
-    self.path = '/'.join(self.fPath.split('/')[:-1])
-    self.format = '.' + self.fPath.split('.')[-1]
+    print(self.fPath)
+    if self.fPath:
+        self.path = '/'.join(self.fPath.split('/')[:-1])
+        self.format = '.' + self.fPath.split('.')[-1]
